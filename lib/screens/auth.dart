0eba63f844c1ea3695ec48783363ad1bfd3caeec
Darpan Khatri <darpankhatri2004@gmail.com>
2024-05-09 16:32:05 +0530

    feat:Add login functionality

--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -9,6 +9,17 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
+  final _form = GlobalKey<FormState>();
+  var _isLogin = true;
+  var _enteredEmail = '';
+  var _enteredPassword = '';
+  void _submit() {
+    final isValid = _form.currentState!.validate();
+    if (isValid) {
+      _form.currentState!.save();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -23,7 +34,7 @@ class _AuthScreenState extends State<AuthScreen> {
                 margin:
                     EdgeInsets.only(top: 30, bottom: 20, left: 20, right: 20),
                 width: 200,
-                child: Image.asset('assets/images.chat.png'),
+                child: Image.asset('assets/images/chat.png'),
               ),
               Card(
                 margin: EdgeInsets.all(20),
@@ -31,6 +42,7 @@ class _AuthScreenState extends State<AuthScreen> {
                   child: Padding(
                     padding: EdgeInsets.all(16),
                     child: Form(
+                      key: _form,
                       child: Column(
                         mainAxisSize: MainAxisSize.min,
                         children: [
@@ -40,14 +52,55 @@ class _AuthScreenState extends State<AuthScreen> {
                             ),
                             keyboardType: TextInputType.emailAddress,
                             autocorrect: false,
+                            validator: (value) {
+                              if (value == null ||
+                                  value.trim().isEmpty ||
+                                  !value.contains('@')) {
+                                return 'Please enter a valid email address';
+                              }
+                              return null;
+                            },
                             textCapitalization: TextCapitalization.none,
+                            onSaved: (newValue) {
+                              _enteredEmail = newValue!;
+                            },
                           ),
                           TextFormField(
                             decoration: InputDecoration(
                               labelText: 'Password',
                             ),
                             obscureText: true,
+                            validator: (value) {
+                              if (value == null || value.trim().length < 6) {
+                                return 'Password must be atleast 6 characters long.';
+                              }
+                              return null;
+                            },
+                            onSaved: (newValue) {
+                              _enteredPassword = newValue!;
+                            },
                           ),
+                          const SizedBox(
+                            height: 12,
+                          ),
+                          ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Theme.of(context)
+                                    .colorScheme
+                                    .primaryContainer,
+                              ),
+                              onPressed: _submit,
+                              child: Text(_isLogin ? 'Login' : 'Signup')),
+                          TextButton(
+                            onPressed: () {
+                              setState(() {
+                                _isLogin = !_isLogin;
+                              });
+                            },
+                            child: Text(_isLogin
+                                ? 'Create an account'
+                                : 'I already have an account.'),
+                          )
                         ],
                       ),
                     ),
